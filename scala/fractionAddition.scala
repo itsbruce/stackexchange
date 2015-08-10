2c394d4eb5324087c455c039aed7802b98b34a3f
Bruce Richardson <it.is.bruce@gmail.com>
2015-08-10 19:09:42 +0100

    Added Fraction class to Numeric type class

--- scala/fractionAddition.scala
+++ scala/fractionAddition.scala
@@ -25,7 +25,25 @@ class Fraction(c: BigInt, d: BigInt) {
 
 object Fraction {
   def apply(c: BigInt, d: BigInt) = new Fraction(c, d)
-  // Implicit classes added in Scala 2.10
+  def apply(i: Int) = new Fractioin(i, 1)
+
+  implicit object NumericFraction extends Numeric[Fraction] {
+    // Required by scala.math.Numeric
+    def fromInt(x: Int): Fraction = Fraction(x, 1)
+    def minus(x: Fraction, y: Fraction): Fraction = x + negate(y)
+    def negate(x: Fraction) = Fraction( - x.counter, x.denominator)
+    def plus(x: Fraction, y: Fraction): Fraction = x + y
+    def times(x: Fraction, y: Fraction): Fraction =
+      Fraction(x.counter * y.counter, x.denominator * y.denominator)
+    def toDouble(x: Fraction): Double = x.counter.toDouble / x.denominator.toDouble
+    def toFloat(x: Fraction): Float = x.counter.toFloat / x.denominator.toFloat
+    def toInt(x: Fraction): Int = (x.counter / x.denominator).toInt
+    def toLong(x: Fraction): Long = (x.counter / x.denominator).toLong
+    // Required by scala.math.Ordering
+    def compare(x: Fraction, y: Fraction) = (x.counter * y.denominator) compare (y.counter * x.denominator)
+  }
+
+  // Allows Int + Fraction
   implicit class FractionalInt(i: Int) {
     def + (that: Fraction) = that + i
   }
