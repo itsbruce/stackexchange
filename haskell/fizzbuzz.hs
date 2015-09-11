c58d408075516998f4b07e677c0625f5e680df59
Bruce Richardson <it.is.bruce@gmail.com>
2015-09-12 00:05:25 +0100

    Haskell code

---
+++ haskell/fizzbuzz.hs
@@ -0,0 +1,13 @@
+import Data.List (unfoldr)
+
+toFizzBuzz :: Int -> Int -> [String]
+toFizzBuzz start count = 
+    let 
+        fizz = const "fizz"
+        buzz = const "buzz"
+        fizzbuzz = const "fizzbuzz"
+        fizzbuzzFuncs =  cycle [show, show, fizz, show, buzz, fizz, show, show, fizz, buzz, show, fizz, show, show, fizzbuzz]
+        offsetFuncs = drop (mod (start - 1) 15) fizzbuzzFuncs
+        step (i, fs) = Just((head fs) i, (i + 1, tail fs))
+    -- in take count $ zipWith ($) offsetFuncs [start..]
+    in take count $ unfoldr step (start, offsetFuncs)
