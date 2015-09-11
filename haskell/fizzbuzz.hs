c74788fa545a72dc280c32711505c4b5c0070bbb
Bruce Richardson <it.is.bruce@gmail.com>
2015-09-12 00:39:29 +0100

    Code for Code Review question 104322

--- haskell/fizzbuzz.hs
+++ haskell/fizzbuzz.hs
@@ -1,9 +1,10 @@
+// http://codereview.stackexchange.com/questions/104322/a-better-fizzbuzz
+
 import Data.List (unfoldr)
 
 toFizzBuzz :: Int -> Int -> [String]
 toFizzBuzz start count = 
-    let 
-        fizz = const "fizz"
+    let fizz = const "fizz"
         buzz = const "buzz"
         fizzbuzz = const "fizzbuzz"
         fizzbuzzFuncs =  cycle [show, show, fizz, show, buzz, fizz, show, show, fizz, buzz, show, fizz, show, show, fizzbuzz]
