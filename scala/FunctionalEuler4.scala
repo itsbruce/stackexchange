2de65d6fdb2ad8500b21bd30677f0f255018231a
Bruce Richardson <it.is.bruce@gmail.com>
2015-09-26 03:25:28 +0100

    CR 105126 - changed Range to Iterator.from and removed alternative methods which turned out to be more expensive than the simple solution

--- scala/FunctionalEuler4.scala
+++ scala/FunctionalEuler4.scala
@@ -3,9 +3,9 @@
 // This example has a few too many magic numbers.
 
 // Lazily generate all 999 palindromic 6-digit numbers in reverse order
-def ps6 = for (i <- (9 to 1 by -1).iterator;
-              j <- (9 to 0 by -1).iterator;
-              k <- (9 to 0 by -1).iterator)
+def ps6 = for (i <- Iterator.from(9, -1) take 9;
+              j <- Iterator.from(9, -1) take 10;
+              k <- Iterator.from(9, -1) take 10)
               yield (i + 10 * j + 100 * k + 1000 * k + 10000 * j + 100000 * i)
 
 // Even more lazy generation of the 5-digit palindromes
@@ -18,35 +18,6 @@ def ps = (ps6 dropWhile (_ > 999 * 999)) ++ (ps5 takeWhile (_ > 9999))
  * numbers.
  */
 ps find {p =>
-  (999 to 100 by -1).iterator takeWhile (p / _ < 1000) exists (p % _ == 0)
+  Iterator.from(999, -1) take 900 takeWhile (p / _ < 1000) exists (p % _ == 0)
 }
 
-// Lazily generate all products of 3-digit numbers in descending order
-def productsFrom(i: Int): Stream[Int] = {
-  // Merge two descending streams in sorted order
-  def descending(s1: Stream[Int], s2: Stream[Int]): Stream[Int] = {
-    val (higher, lower) = s1 span (_ > s2.head)
-    higher ++ s2.head #:: descending(lower, s2.tail)
-  }
-  val first = i * i
-  first #:: (first - i) #::
-    descending(productsFrom(i - 1), Stream.from(first - 2 * i, -i))
-}
-
-def findBiggestMultiple(pals: Stream[Int], products: Stream[Int]): Option[Int] =
-  pals.headOption flatMap { x =>
-    val rest = products dropWhile (_ > x)
-    rest.headOption flatMap { y =>
-      if (y == x) Some(x) else findBiggestMultiple(pals.tail, rest)
-    }
-  }
-
-
-findBiggestMultiple(ps.toStream, productsFrom(999))
-
-/*
-  def descending(s1: Stream[Int], s2: Stream[Int]): Stream[Int] = {
-    val (higher, lower) = s1 span (_ > s2.head)
-    higher ++ s2.head #:: descending(lower, s2.tail)
-  }
-  */
